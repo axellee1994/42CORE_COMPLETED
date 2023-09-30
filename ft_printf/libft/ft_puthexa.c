e86636a8ee4aa3e64a69ed8bf95a2196174e900c
Axel Lee <axlee@c2r2s7.42singapore.sg>
2023-09-30 17:07:29 +0800

    Added ft_libft

---
+++ ft_printf/libft/ft_puthexa.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthexa.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: axlee <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/23 15:43:22 by axlee             #+#    #+#             */
+/*   Updated: 2023/09/30 16:49:52 by axlee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_puthexa(unsigned long n, char format)
+{
+	int				i;
+	char			*base;
+	unsigned int	num;
+	unsigned int	base_len;
+
+	if (format == 'X')
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789abcdef";
+	i = 0;
+	num = (unsigned int)n;
+	base_len = ft_strlen(base);
+	if (num >= (base_len))
+	{
+		i += ft_puthexa(num / base_len, format);
+		i += ft_putchar(base[num % base_len]);
+	}
+	else
+		i += ft_putchar(base[num % base_len]);
+	return (i);
+}
